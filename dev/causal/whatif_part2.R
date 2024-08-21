021dc1e8062b070b454526287cc005bcd1a9f688
chizhang <andreachizhang@yahoo.com>
2024-08-21 17:57:25 +0200

    two methods

--- dev/causal/whatif_part2.R
+++ dev/causal/whatif_part2.R
@@ -67,7 +67,7 @@ nhefs.nmv$w <- 1 / p.qsmk.obs
 summary(nhefs.nmv$w)
 
 
-# lm ----
+## lm ----
 
 m1 <- lm(wt82_71 ~ qsmk, data = nhefs.nmv)
 summary(m1)
@@ -80,7 +80,7 @@ predict(m1, data.frame(qsmk = 0)) # 1.98
 # beta = 2.54 
 
 
-# lm with weights ----
+## lm with weights ----
 
 ?lm
 
@@ -95,4 +95,78 @@ predict(m1w, data.frame(qsmk = 0)) # 1.77
 
 
 
+# g-formula ----
 
+fit <-
+  glm(
+    wt82_71 ~ qsmk + sex + race + age + I(age * age) + as.factor(education)
+    + smokeintensity + I(smokeintensity * smokeintensity) + smokeyrs
+    + I(smokeyrs * smokeyrs) + as.factor(exercise) + as.factor(active)
+    + wt71 + I(wt71 * wt71) + qsmk * smokeintensity,
+    data = nhefs
+  )
+
+summary(fit)
+
+nhefs$predicted.meanY <- predict(fit, nhefs)
+
+mean(nhefs$predicted.meanY)
+
+# select one person
+nhefs[which(nhefs$seqn == 24770), c(
+  "predicted.meanY",
+  "qsmk",
+  "sex",
+  "race",
+  "age",
+  "education",
+  "smokeintensity",
+  "smokeyrs",
+  "exercise",
+  "active",
+  "wt71"
+)]
+
+# predicted value vs y
+summary(nhefs$predicted.meanY[nhefs$cens == 0])
+summary(nhefs$wt82_71[nhefs$cens == 0])
+
+
+# counterfactual datasets ----
+# create a dataset with 3 copies of each subject
+nhefs$interv <- -1 # 1st copy: equal to original one
+
+interv0 <- nhefs # 2nd copy: treatment set to 0, outcome to missing
+interv0$interv <- 0
+interv0$qsmk <- 0 # A=0 in all rows
+interv0$wt82_71 <- NA
+
+interv1 <- nhefs # 3rd copy: treatment set to 1, outcome to missing
+interv1$interv <- 1
+interv1$qsmk <- 1 # A=1 in all rows
+interv1$wt82_71 <- NA
+
+# use row bind
+onesample <- rbind(nhefs, interv0, interv1) # combining datasets
+
+
+std <- glm(
+  wt82_71 ~ qsmk + sex + race + age + I(age * age)
+  + as.factor(education) + smokeintensity
+  + I(smokeintensity * smokeintensity) + smokeyrs
+  + I(smokeyrs * smokeyrs) + as.factor(exercise)
+  + as.factor(active) + wt71 + I(wt71 * wt71) + I(qsmk * smokeintensity),
+  data = onesample # binded data
+)
+summary(std)
+
+onesample$predicted_meanY <- predict(std, onesample)
+# original data
+mean(onesample[which(onesample$interv == -1), ]$predicted_meanY) # 2.56
+# A=0
+mean(onesample[which(onesample$interv == 0), ]$predicted_meanY) #1.66
+# A=1
+mean(onesample[which(onesample$interv == 1), ]$predicted_meanY) # 5.17
+
+# difference: (treatment effect)
+# 5.17 - 1.66
